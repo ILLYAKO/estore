acdfc3f9a45c271c0021fc8f0d1090c631fdcc70
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-11-17 20:03:23 -0600

    2020-11-16

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
+import PropTypes from "prop-types";
 
 export default class Product extends Component {
   render() {
@@ -9,32 +10,41 @@ export default class Product extends Component {
     return (
       <ProductWrapper className=" col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-          <div
-            className="img-container p-5"
-            onClick={() => {
-              console.log("clicked");
-            }}
-          >
-            <Link to="/details">
-              <img src={img} alt="product" className="card-img-top" />
-            </Link>
-            <button
-              className="cart-btn"
-              disabled={inCart ? true : false}
-              onCLick={() => {
-                console.log("added to cart");
-              }}
-            >
-              {inCart ? (
-                <p className="text-capitalize mb-0" disabled>
-                  {" "}
-                  in cart
-                </p>
-              ) : (
-                <i className="fas fa-cart-plus"></i>
-              )}
-            </button>
-          </div>
+          <ProductConsumer>
+            {(value) => (
+              <div
+                className="img-container p-5"
+                onClick={() => {
+                  // value.handleDetail(id);
+                  value.handleDetail(id);
+                  console.log("onClick-handleDetail");
+                }}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => {
+                    // value.addToCart(id);
+                    value.addToCart(id);
+                    value.openModal(id);
+                    console.log("onClick-addToCart");
+                  }}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      in cart
+                    </p>
+                  ) : (
+                    <i className="fas fa-cart-plus"></i>
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
+
           {/* card footer*/}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
@@ -48,6 +58,16 @@ export default class Product extends Component {
     );
   }
 }
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool,
+  }).isRequired,
+};
 
 const ProductWrapper = styled.div`
   .card {
@@ -88,5 +108,15 @@ const ProductWrapper = styled.div`
     color: var(--mainWhite);
     font-size: 1.4rem
     border-radius: 0.5rem 0 0 0;
+    transform:translate(100%, 100%);
+    transition: all 1s linear;
+
+  }
+  .img-container:hover .cart-btn{
+    transform: translate(0,0)
+  }
+  .cart-btn:hover{
+    color: var(--mainBlue);
+    cursor: pointer;
   }
 `;
